bfcaa3116df0dcf9e946d8c466be34b0ebff63af
Matthieu_772 <matthieu.dietrich77@gmail.com>
2025-10-15 16:47:01 +0200

    structure jsx de la landing

--- app-web/src/App.jsx
+++ app-web/src/App.jsx
@@ -8,7 +8,26 @@ function App() {
 
   return (
     <>
-    <h1 className="bg-red-500">Titre</h1>
+    <section>
+     <h1 className="text-[#7CA982] text-3xl font-bold">M2I Le five entre amis !</h1>
+    </section>
+
+    <section>
+      <p className="text-[#7CA982] text-5xl font-bold">Un ballon, <br /> des potes, <br />des émotions:<br /> rejoins le match !</p>
+    </section>
+
+    <section>
+      <button 
+      type="button"
+      className="bg-linear-to-r from-[#7CA982] to-[#a4ebad] rounded-xl ml-10 text-white p-1"
+      >
+        Rejoins-nous !
+      </button>
+    </section>
+
+    <section>
+      <img src="../public/img/landing-img.jpeg" alt="football is life" />
+    </section>
     </>
   )
 }

8e8cfe6e3fbf70d5424a43ae91d31a7c7caa760a
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2025-10-19 18:48:26 +0200

    ajout du composant formulaire

--- app-web/src/App.jsx
+++ app-web/src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import '../public/css/style.css'
+import FormulaireInscription from "./FormulaireInscription";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1 className="bg-red-500">Titre</h1>
+      <FormulaireInscription />
     </>
   )
 }

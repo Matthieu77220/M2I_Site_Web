be8f5839c38966d1c3839454bca0b91761b03be5
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2025-10-23 17:41:01 +0200

    création de la NavBar

--- app-web/src/App.jsx
+++ app-web/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import '../public/css/style.css'
+import NavBar from './components/navBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className="bg-red-500">Titre</h1>
+      <NavBar />
     </>
   )
 }

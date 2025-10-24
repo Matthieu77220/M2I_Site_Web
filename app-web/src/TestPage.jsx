d2e29b70171aef9e5e38c9d6d37f306628483398
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2025-10-24 15:10:55 +0200

    🚨 Je viens de créer une nouvelle branche sans faire exprès... Il y a 2 branch NavBar

--- app-web/src/TestPage.jsx
+++ app-web/src/TestPage.jsx
@@ -8,7 +8,7 @@ function test() {
     return ( 
         <>
             <div className='flex'>
-                <NavBar open={open} setOpen={setOpen} />
+                <NavBar open={open} setOpen={setOpen} /> {/* Passe en props les éléments du UseStat (open,setOpen) */}
             </div>
             <div className= {`duration-500 ${open ? "pl-60" : "pl-[72px]"}`}>
                 <p>hello world</p>

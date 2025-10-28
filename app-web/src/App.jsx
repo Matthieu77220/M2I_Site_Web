054816ade25fa55d5dd8af830c1aec4d0446e639
Matthieu_772 <matthieu.dietrich77@gmail.com>
2025-10-28 16:08:05 +0100

    :goal_net: landing page finalisé

--- app-web/src/App.jsx
+++ app-web/src/App.jsx
@@ -4,27 +4,55 @@ function App() {
 
   return (
     <>
-    <section className="flex flex-col-reverse">
+    <section className="
+        flex flex-col-reverse
+        
+        md:flex md:flex-row
+        ">
 
-      <section className="flex flex-col">
-
-        <section className="flex">
-        <h1 className="text-[#7CA982] text-lg font-bold">
+      <section className="
+      flex flex-col
+      md:w-[50vw] md:mt-[25vh]
+      ">
+        
+        <section className="flex justify-center text-center">
+        <h1 className="
+        font-spartan font-semibold text-2xl text-[#7CA982]
+        md:text-5xl
+        lg:text-6xl
+        xl:text-7xl
+        ">
             M2I Le five entre amis !
           </h1>
         </section>
 
-        <section className="flex mt-10">
-          <p className="text-[#7CA982] text-base font-semibold">Un ballon, <br /> des potes, <br />des émotions:<br /> rejoins le match !</p>
+        <section className="flex mt-10 justify-center text-center">
+          <p className="
+          text-[#7CA982] text-lg font-roboto
+          md:text-4xl
+          lg:text-5xl
+          xl:text-6xl
+          ">
+            Un ballon, <br /> des potes, <br />des émotions:<br /> rejoins le match !</p>
         </section>
 
         <section className="flex justify-center">
 
           <button 
           type="button"
-          className="bg-linear-to-r from-[#7CA982] to-[#a4ebad] rounded-4xl mt-10 text-white p-4 w-7/12"
+          className="
+          bg-linear-to-r from-[#7CA982] to-[#a4ebad] mt-10 rounded-full text-white p-4 w-7/12 duration-300 ease-in
+          lg:p-5
+          xl:p-6
+          hover:opacity-80 hover:cursor-pointer hover:w-8/12 hover:bg-linear-to-r hover:from-[#a4ebad] hover:to-[#7CA982] hover:duration-300
+          "
           >
-            Rejoins-nous !
+            <p className="
+            md:text-2xl 
+            lg:text-2xl 
+            xl:text-3xl
+            ">
+              Rejoins-nous !</p>
           </button>
 
         </section>
@@ -36,7 +64,10 @@ function App() {
           <img 
           src="./src/img/landing-img.png" 
           alt="football is life" 
-          className="mb-5 h-auto"
+          className="
+          mb-5 object-cover w-full h-[66vh]
+          md:object-cover md:w-[50vw] md:h-screen md:mb-0 md:right-full
+          "
           />
 
         </section>
@@ -47,3 +78,7 @@ function App() {
 }
 
 export default App
+
+
+//clip-path: polygon(calc(100% - 15px) 0, 100% 15px, 100% 100%, 15px 100%, 0 calc(100% - 15px), 0 0);
+ //transition: background .25s ease, transform .2s ease;

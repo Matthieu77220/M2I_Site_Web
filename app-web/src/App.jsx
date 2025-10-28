c7cd5b0d459e380e5276a204cb589a47591e2275
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2025-10-28 17:25:39 +0100

    création de la page Mot de passe oublié  [Regex]

--- app-web/src/App.jsx
+++ app-web/src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import '../public/css/style.css'
-import FormulaireInscription from "./FormulaireInscription";
+import MotDePasseOublie from './MotDePasseOublie';
 
 function App() {
   return (
-    <div className='flex flex-col justify-center lg:h-screen bg-[#5E856B]'>
-      <FormulaireInscription />
-    </div>
+      <MotDePasseOublie />
   )
 }
 

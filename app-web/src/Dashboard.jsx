c9e803d6084ec36cf4dff223da6ad2987ba2d772
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2025-11-04 17:41:49 +0100

    ajout du responsive sur le dashboard

--- app-web/src/Dashboard.jsx
+++ app-web/src/Dashboard.jsx
@@ -13,35 +13,30 @@ function Dashboard() {
 
             <div className={`duration-500 ${open ? "pl-60" : "pl-[72px]"} min-h-screen bg-[#f3f8f4]`}>
                 {/* --- En-tête du tableau de bord --- */}
-                <div className="p-8">
+                <div className="">
 
                     {/* --- Boutons pour changer de tableau --- */}
-                   <div className="h-[700px] w-full flex items-center justify-center gap-16 mb-12">
-    <button
-        onClick={() => setActiveTab('licence')}
-        className={`w-[500px] h-[300px] rounded-2xl font-bold text-2xl shadow-lg transition-transform ${
-            activeTab === 'licence'
-                ? 'bg-[#7bd0a0] text-white scale-105'
-                : 'bg-gray-200 hover:scale-105'
-        }`}
-    >
-        Licence
-    </button>
+                    <div className="h-screen w-full flex flex-col min-md:flex-row items-center justify-center gap-16">
+                        <button
+                            onClick={() => setActiveTab('licence')}
+                            className={`w-lg h-80 max-w-1/2 max-h-2/5 rounded-2xl m-5 font-bold text-2xl shadow-lg transition-transform ${activeTab === 'licence'
+                                    ? 'bg-[#7bd0a0] text-white scale-105'
+                                    : 'bg-gray-200 hover:scale-105'
+                                }`}
+                        >
+                            Licence
+                        </button>
 
-    <button
-        onClick={() => setActiveTab('frais')}
-        className={`w-[500px] h-[300px] rounded-2xl font-bold text-2xl shadow-lg transition-transform ${
-            activeTab === 'frais'
-                ? 'bg-[#7bd0a0] text-white scale-105'
-                : 'bg-gray-200 hover:scale-105'
-        }`}
-    >
-        Frais
-    </button>
-</div>
-
-
-                    
+                        <button
+                            onClick={() => setActiveTab('frais')}
+                            className={`w-lg h-80 max-w-1/2 max-h-2/5 rounded-2xl m-5 font-bold text-2xl shadow-lg transition-transform ${activeTab === 'frais'
+                                    ? 'bg-[#7bd0a0] text-white scale-105'
+                                    : 'bg-gray-200 hover:scale-105'
+                                }`}
+                        >
+                            Frais
+                        </button>
+                    </div>
                 </div>
             </div>
         </>

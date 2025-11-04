51390b0bac6247ab91a939001cba63811cb35ca5
Matthieu_772 <matthieu.dietrich77@gmail.com>
2025-11-04 09:34:27 +0100

    modif

--- app-web/src/main.jsx
+++ app-web/src/main.jsx
@@ -1,10 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import './css/style.css'
-=======
-// import './style.css'
->>>>>>> connexion
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

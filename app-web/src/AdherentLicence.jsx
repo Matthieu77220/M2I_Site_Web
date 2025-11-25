8753380a959d96dfe5b1ca3e52f7caa1407f649a
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2025-11-25 12:15:07 +0100

    création d'un user fictive pour map les données

--- app-web/src/AdherentLicence.jsx
+++ app-web/src/AdherentLicence.jsx
@@ -4,6 +4,19 @@ import NavBar from './components/navBar'
 
 function AdherentLicence() {
     const [open, setOpen] = useState(true)
+
+    //Exemple d'un user 
+    const user = [
+        {
+            id: 1,
+            nom: "Dupont Antoine",
+            club: "FC Esbly",
+            dateNaissance : "12/01/2001",
+            numeroLicence : "X2395283",
+            dateDebutLicence : "14/10/2025",
+            finDebutLicence: "14/10/2028"
+        }
+    ]
     
     return ( 
         <>
@@ -26,25 +39,30 @@ function AdherentLicence() {
                                     <image className='w-full h-full object-cover'></image>
                                 </div>
 
-                    
-                                <div className='flex-1 bg-white rounded-lg p-6 shadow-md'>
-                                    <h3 className='font-bold text-xl mb-4 text-gray-800'>Dupont Antoine</h3>
-                                    <p className='mb-2 text-gray-700'><strong>Club :</strong> FC Esbly</p>
-                                    <p className='mb-2 text-gray-700'><strong>Né le :</strong> 12/01/2000</p>
-                                    <p className='text-gray-700'><strong>Numéro :</strong> XX</p>
-                                </div>
+                                {user.map((element) => (
+                                   <div key={element.id} className='flex-1 bg-white rounded-lg p-6 shadow-md'>
+                                        <h3 className='font-bold text-xl mb-4 text-gray-800'> {element.nom}</h3>
+                                        <p className='mb-2 text-gray-700'><strong>Club :</strong> {element.club}</p>
+                                        <p className='mb-2 text-gray-700'><strong>Né le :</strong> {element.dateNaissance}</p>
+                                        <p className='text-gray-700'><strong>Numéro :</strong> {element.numeroLicence}</p>
+                                    </div>
+                                    ))
+                                }
                             </div>
 
-                       
-                            <div className='bg-white rounded-lg p-6 shadow-md mb-8'>
-                                <p className='mb-2 text-gray-700'><strong>Délivrée le :</strong> 14/10/2025</p>
-                                <p className='text-gray-700'><strong>Date de validité :</strong> 14/10/2028</p>
-                            </div>
+                            {user.map((element) => (
+                                <div key={element.id} className='bg-white rounded-lg p-6 shadow-md mb-8'>
+                                    <p className='mb-2 text-gray-700'><strong>Délivrée le :</strong> {element.dateDebutLicence}</p>
+                                    <p className='text-gray-700'><strong>Date de validité :</strong> {element.finDebutLicence}</p>
+                                </div>
+                            ))
+
+                            }
 
                         
                             <div className='flex justify-center'>
-                                <button className='bg-[#7CA982] hover:bg-[#a4ebad] text-white font-semibold py-3 px-8 rounded-lg shadow-lg transition duration-200 transform hover:scale-105'>
-                                Sauvegarder
+                                <button className='bg-green-900 hover:bg-green-900/40 cursor-pointer text-white font-semibold py-3 px-8 rounded-lg shadow-lg transition duration-200 transform hover:scale-105'>
+                                    Sauvegarder
                                 </button>
                             </div>
                         </div>

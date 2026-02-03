ebe9fa6167380823db480fa33ef1de40b7bdc730
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2026-02-03 17:35:42 +0100

    ajout indentation

--- app-web/src/components/NavBarAdmin.jsx
+++ app-web/src/components/NavBarAdmin.jsx
@@ -1,4 +1,4 @@
-import {useNavigate} from 'react-router';
+import { useNavigate } from 'react-router';
 
 
 import { MdMenuOpen } from "react-icons/md";
@@ -12,75 +12,75 @@ import { MdOutlineLogout } from "react-icons/md";
 import axios from "axios";
 
 
-function NavBarAdmin({open, setOpen}) {
+function NavBarAdmin({ open, setOpen }) {
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    async function deconnexionFunction() {
+  async function deconnexionFunction() {
 
-      try {
-        await axios.post("http://localhost:3000/api/auth/deconnexion", {}, {withCredentials: true})
-        navigate('/Connexion')
+    try {
+      await axios.post("http://localhost:3000/api/auth/deconnexion", {}, { withCredentials: true })
+      navigate('/Connexion')
 
-      }catch (err) {
-        console.log(err)
-      }
-    
+    } catch (err) {
+      console.log(err)
+    }
+
   }
 
-    return (  
-        <>
+  return (
+    <>
 
-            {/*Navbar*/}
-            <nav className={`fixed left-0 top-0 h-screen p-2 flex flex-col duration-500 bg-[#7CA982] text-white ${open ? 'w-60' : 'w-18'}`}>
+      {/*Navbar*/}
+      <nav className={`fixed left-0 top-0 h-screen p-2 flex flex-col duration-500 bg-[#7CA982] text-white ${open ? 'w-60' : 'w-18'}`}>
 
-                {/* Menu Burger */}
-                <div className='p-2 h-20 items-center'>
-                  <MdMenuOpen size={34} className={`duration-500 cursor-pointer ${!open && ' rotate-180'}`} onClick={() => setOpen(!open)} />
-                </div>
+        {/* Menu Burger */}
+        <div className='p-2 h-20 items-center'>
+          <MdMenuOpen size={34} className={`duration-500 cursor-pointer ${!open && ' rotate-180'}`} onClick={() => setOpen(!open)} />
+        </div>
 
-                {/* Icon */}
-                <ul className='flex flex-col'>
+        {/* Icon */}
+        <ul className='flex flex-col'>
 
-                  <li onClick={() =>{ navigate("/PannelAdmin")}} className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'>
-                    <div><MdOutlineDashboard size={30} /></div>
-                    <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-700 font-semibold `}>Dashboard</p>
-                  </li>
+          <li onClick={() => { navigate("/PannelAdmin") }} className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'>
+            <div><MdOutlineDashboard size={30} /></div>
+            <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-700 font-semibold `}>Dashboard</p>
+          </li>
 
-                  <li  onClick={() =>{navigate("/AdminStats")}}className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'>
-                    <div><FaUserCircle size={30} /></div>
-                    <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-900 font-semibold `}>Statistiques</p>
-                  </li>
+          <li onClick={() => { navigate("/AdminStats") }} className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'>
+            <div><FaUserCircle size={30} /></div>
+            <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-900 font-semibold `}>Statistiques</p>
+          </li>
 
-                  <li onClick={() =>{navigate('/Equipements')}} className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'>
-                    <div><CiDumbbell size={30} /></div>
-                    <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1100 font-semibold `}>Equipements</p>
-                  </li>
+          <li onClick={() => { navigate('/Equipements') }} className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'>
+            <div><CiDumbbell size={30} /></div>
+            <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1100 font-semibold `}>Equipements</p>
+          </li>
 
-                  <li onClick={() =>{navigate('/AdminTerrains')}} className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'>
-                    <div><GiSoccerField size={30} /></div>
-                    <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1300  font-semibold `}>Terrains</p>
-                  </li>
+          <li onClick={() => { navigate('/AdminTerrains') }} className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'>
+            <div><GiSoccerField size={30} /></div>
+            <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1300  font-semibold `}>Terrains</p>
+          </li>
 
-                  <li onClick={() =>{navigate("/AdminUsers")}} className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'>
-                    <div><FaUsersCog size={30} /></div>
-                    <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1500 font-semibold`}>Utilisateurs</p>
-                  </li>
-                </ul>
+          <li onClick={() => { navigate("/AdminUsers") }} className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'>
+            <div><FaUsersCog size={30} /></div>
+            <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1500 font-semibold`}>Utilisateurs</p>
+          </li>
+        </ul>
 
-                {/* footer */}
-                <div className='flex absolute bottom-0 items-center mt-5 mb-5 gap-5 w-55 p-2 cursor-pointer hover:bg-green-800 hover:[max-width:calc(100%-1rem)] hover:rounded-md' onClick={() => deconnexionFunction()}>
-                  <div className="flex gap-5"><MdOutlineLogout size={30} />
-                    <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1500 font-semibold`}>Déconnexion</p>
-                  </div>
-                </div>
-            </nav>
+        {/* footer */}
+        <div className='flex absolute bottom-0 items-center mt-5 mb-5 gap-5 w-55 p-2 cursor-pointer hover:bg-green-800 hover:[max-width:calc(100%-1rem)] hover:rounded-md' onClick={() => deconnexionFunction()}>
+          <div className="flex gap-5"><MdOutlineLogout size={30} />
+            <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1500 font-semibold`}>Déconnexion</p>
+          </div>
+        </div>
+      </nav>
 
-            {/*PannelAdmin*/}
+      {/*PannelAdmin*/}
 
 
-        </>
-    );
+    </>
+  );
 }
 
 export default NavBarAdmin;
--- app-web/src/components/navBar.jsx
+++ app-web/src/components/navBar.jsx
@@ -7,72 +7,73 @@ import { FaIdCard } from "react-icons/fa";
 import { ImStatsBars } from "react-icons/im";
 import { CiSettings } from "react-icons/ci";
 import { MdOutlineLogout } from "react-icons/md";
+
 import axios from "axios";
 
 
 
-function NavBar({open,setOpen}) {// Récupération des useStat depuis le parent
+function NavBar({ open, setOpen }) {// Récupération des useStat depuis le parent
 
   const navigate = useNavigate();
 
-  async function deconexionFunction() {
+  async function deconnexionFunction() {
 
     try {
-      await axios.post("http://localhost:3000/api/auth/deconnexion", {}, {withCredentials: true})
+      await axios.post("http://localhost:3000/api/auth/deconnexion", {}, { withCredentials: true })
       navigate('/Connexion')
 
-    }catch (err) {
+    } catch (err) {
       console.log(err)
     }
-    
+
   }
 
-    return (
-        <>
-            <nav className={`fixed left-0 top-0 h-screen p-2 flex flex-col duration-500 bg-[#7CA982] text-white ${open ? 'w-60' : 'w-18'}`}>
+  return (
+    <>
+      <nav className={`fixed left-0 top-0 h-screen p-2 flex flex-col duration-500 bg-[#7CA982] text-white ${open ? 'w-60' : 'w-18'}`}>
 
-                {/* Menu Burger */}
-                <div className='p-2 h-20 items-center'>
-                  <MdMenuOpen size={34} className={`duration-500 cursor-pointer ${!open && ' rotate-180'}`} onClick={() => setOpen(!open)} />
-                </div>
+        {/* Menu Burger */}
+        <div className='p-2 h-20 items-center'>
+          <MdMenuOpen size={34} className={`duration-500 cursor-pointer ${!open && ' rotate-180'}`} onClick={() => setOpen(!open)} />
+        </div>
 
-                {/* Icon */}
-                <ul className='flex flex-col'>
+        {/* Icon */}
+        <ul className='flex flex-col'>
 
-                  <li className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'
-                       onClick={() => navigate('/Dashboard')}>
-                    <div><MdOutlineDashboard size={30} /></div>
-                    <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-700 font-semibold `}>Dashboard</p>
-                  </li>
+          <li className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'
+            onClick={() => navigate('/Dashboard')}>
+            <div><MdOutlineDashboard size={30} /></div>
+            <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-700 font-semibold `}>Dashboard</p>
+          </li>
 
-                  <li className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'
-                      onClick={() => navigate('/AdherentLicence')}>
-                    <div><FaIdCard size={30} /></div>
-                    <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-900 font-semibold `}>Licence</p>
-                  </li>
+          <li className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'
+            onClick={() => navigate('/AdherentLicence')}>
+            <div><FaIdCard size={30} /></div>
+            <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-900 font-semibold `}>Licence</p>
+          </li>
 
-                  <li className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'
-                      onClick={() => navigate('/Statistique')}>
-                    <div><ImStatsBars size={30} /></div>
-                    <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1100 font-semibold `}>Statistique</p>
-                  </li>
+          <li className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'
+            onClick={() => navigate('/Statistique')}>
+            <div><ImStatsBars size={30} /></div>
+            <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1100 font-semibold `}>Statistique</p>
+          </li>
 
-                  <li className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'
-                       onClick={() => navigate('/profile')}>
-                    <div><CiSettings size={30} /></div>
-                    <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1500 font-semibold`}>Paramètre</p>
-                  </li>
-                </ul>
+          <li className='px-3 py-2 my-2 hover:bg-green-800 hover:rounded-md cursor-pointer flex gap-5 items-center relative'
+            onClick={() => navigate('/profile')}>
+            <div><CiSettings size={30} /></div>
+            <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1500 font-semibold`}>Paramètre</p>
+          </li>
+        </ul>
 
-                {/* footer */}
-                <div className='flex absolute bottom-0 items-center mt-5 mb-5 gap-5 w-55 p-2 cursor-pointer hover:bg-green-800 hover:[max-width:calc(100%-1rem)] hover:rounded-md' onClick={() => deconexionFunction()}>
-                  <div className="flex gap-5"><MdOutlineLogout size={30} />
-                    <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1500 font-semibold`}>Déconnexion</p>
-                  </div>
-                </div>
-            </nav>
-        </>
-     );
+        {/* footer */}
+        <div className='flex absolute bottom-0 items-center mt-5 mb-5 gap-5 w-55 p-2 cursor-pointer hover:bg-green-800 hover:[max-width:calc(100%-1rem)] hover:rounded-md' onClick={() => deconnexionFunction()}>
+          <div className="flex gap-5"><MdOutlineLogout size={30} />
+            <p className={`${!open && 'w-0 translate-x-24'} overflow-hidden duration-1500 font-semibold`}>Déconnexion</p>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default NavBar;
